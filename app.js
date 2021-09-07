1163b34c06c6bdce0ea8d921c50735c2dfe7160c
punIN10ded <imnotfreenow@gmail.com>
2021-09-07 12:30:43 +0530

    added keyframes

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const tagline = document.querySelector('.tagline');
+const logo = document.querySelectorAll('.logo');
 const subtagline = document.querySelector('.sub-tagline');
+const navbar = document.querySelector('.navbar');
+const logoimg = document.querySelector('.logo-img');
+const about = document.querySelector('#about');
+const aboutbg = document.querySelector('aboutbg');
 
 const tl = new TimelineMax();
 
-tl.fromTo(tagline,1, {height:"0%"},{height:"100%", ease: Power2.easeInOut})
-.fromTo(tagline,1, {width:"80%"},{width:"100%", ease: Power2.easeInOut});
+tl.fromTo(navbar,1,{opacity:0},{opacity:1,ease: Power2.easeInOut})
+.fromTo(tagline,1.2,{opacity:0},{opacity:1,ease: Power2.easeInOut});
