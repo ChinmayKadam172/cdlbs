c59125a10a970f22ae1d80179bc5d41c2a4516ad
ChinmayKadam172 <57569079+ChinmayKadam172@users.noreply.github.com>
2021-10-04 11:56:10 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -10,3 +10,22 @@ const tl = new TimelineMax();
 
 tl.fromTo(navbar,1,{opacity:0},{opacity:1,ease: Power2.easeInOut})
 .fromTo(tagline,1.2,{opacity:0},{opacity:1,ease: Power2.easeInOut});
+
+// buttonscrolltoTop
+const btnscroll = document.querySelector("#btnscroll");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      btnscroll.style.display = "block";
+    } else {
+      btnscroll.style.display = "none";
+    }
+  }
+btnscroll.addEventListener("click", function() {
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior:"smooth"
+    });
+
+});
